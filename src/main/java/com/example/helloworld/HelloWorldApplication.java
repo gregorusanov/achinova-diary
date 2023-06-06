fb09b8e715abab8d1e80d77ebf7f3402366a15d1
Гиляна <gilya199408@gmail.com>
2023-06-06 15:29:47 +0200

    new

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class HelloWorldApplication {
-    @RequestMapping("/home")
+    @RequestMapping("/")
     String home(){
         return "Hello, World!";
     }
