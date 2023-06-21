29238654b710e180a0b41d11bdde83983f3b812d
Гиляна <gilya199408@gmail.com>
2023-06-21 18:57:47 +0200

    draft

---
+++ src/test/java/com/ghilly/controller/CountryControllerTest.java
@@ -0,0 +1,84 @@
+package com.ghilly.controller;
+
+import com.ghilly.classes.Country;
+import com.ghilly.service.CountryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class CountryControllerTest {
+
+    private CountryController controller;
+    private int id;
+    private String name;
+
+    @BeforeEach
+    void init() {
+        controller = new CountryController(mock(CountryService.class));
+        id = 1;
+        name = "USSR";
+    }
+
+
+    @Test
+    void createCountry() {
+        controller.create(name);
+
+        assertAll(
+                () -> verify(service).add(name),
+                () -> verifyNoMoreInteractions(service)
+        );
+    }
+
+    @Test
+    void getCountries() {
+        controller.getCountries();
+
+        assertAll(
+                () -> verify(service).receiveList(),
+                () -> verifyNoMoreInteractions(service)
+        );
+    }
+
+    @Test
+    void getCountry() {
+        when(service.receiveCountry(eq(id))).thenReturn(name);
+        String expected = controller.getCountry(id);
+
+        assertAll(
+                () -> assertEquals(expected, name),
+                () -> verify(service).receiveCountry(anyInt()),
+                () -> verifyNoMoreInteractions(service)
+        );
+    }
+
+    @Test
+    void updateCountry() {
+        String newName = "Russia";
+
+        controller.update(id, newName);
+
+        assertAll(
+                () -> verify(service).upgrade(id, newName),
+                () ->verifyNoMoreInteractions(service)
+        );
+
+    }
+
+    @Test
+    void deleteCountry() {
+        controller.delete(id);
+
+        assertAll(
+                () -> verify(service).clear(id),
+                () -> verifyNoMoreInteractions(service)
+        );
+
+    }
+}
