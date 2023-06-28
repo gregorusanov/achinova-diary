6e60710a6453c7d92a5999afb95168bdbfc25692
Гиляна <gilya199408@gmail.com>
2023-06-28 14:36:59 +0200

    The connection and entity were created

--- src/main/java/com/ghilly/configuration/TravelDiaryConfiguration.java
+++ src/main/java/com/ghilly/configuration/TravelDiaryConfiguration.java
@@ -5,12 +5,24 @@ import com.ghilly.repository.CountryRepository;
 import com.ghilly.repository.CountryRepositoryRest;
 import com.ghilly.service.CountryService;
 import com.ghilly.service.CountryServiceRest;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class TravelDiaryConfiguration {
 
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432");
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
     @Bean
     public CountryRepositoryRest repositoryRest() {
         return new CountryRepositoryRest();
