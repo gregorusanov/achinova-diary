33bab7e400c0dd0b7c66891847ad92162719e7b7
Гиляна <gilya199408@gmail.com>
2023-07-20 16:57:21 +0200

    .

--- src/main/java/com/ghilly/service/CountryServiceRest.java
+++ src/main/java/com/ghilly/service/CountryServiceRest.java
@@ -23,8 +23,6 @@ public class CountryServiceRest implements CountryService {
 
     @Override
     public Country create(String countryName) {
-        countryName = countryName.replaceAll("[^a-zA-Z]", "");
-        if(countryName.isEmpty()) throw new IllegalArgumentException("This is field should not be empty!");
         if (repository.findByName(countryName).isPresent()) {
             throw new NameAlreadyExistsException("The country with this name " + countryName + " already exists.");
         }
